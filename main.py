7c9b7e580220543a80eadbaf3dbcdc99663003fc
The Emeryverse <server@emeryverse.com>
2026-05-20 20:37:46 -0500

    Update main.py

--- main.py
+++ main.py
@@ -738,8 +738,9 @@ async def emery_engine(history_buffer, model_to_use=MODEL_ID):
             "think": True, # <-- Forced to True to guarantee Ollama calculates reasoning
             "options": {
                 "num_ctx": ctx_size,
-                "temperature": 0.8,
-                "top_p": 0.9,
+                "temperature": 1.0,
+                "top_p": 0.95,
+                "top_k": 64,
             }
         }
         
